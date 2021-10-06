9f276b7dfcf1f784db543537201955c084c80d82
bipulkmr-crypto <bipul1707@gmail.com>
2021-10-06 15:06:01 +0530

    updated indentation

--- src/main/java/PanoViewer/gui/Menu.java
+++ src/main/java/PanoViewer/gui/Menu.java
@@ -3,33 +3,33 @@ package PanoViewer.gui;
 import javax.swing.*;
 
 public class Menu extends JMenuBar {
-    private  JMenu File;//creating menu objects
-    private JMenu  Help;//creating menu objects
-    private JMenuItem Open, Exit;//creating menuitem objects
-    private JMenuItem About;//creating menuitem objects
-    private static Menu instance;//creating a menu instance
-    //private constructor for implementing singleton design principle
-    private Menu() {
+  private  JMenu File;//creating menu objects
+  private JMenu  Help;//creating menu objects
+  private JMenuItem Open, Exit;//creating menuitem objects
+  private JMenuItem About;//creating menuitem objects
+  private static Menu instance;//creating a menu instance
+  //private constructor for implementing singleton design principle
+  private Menu() {
 //        menuBar=new JMenuBar();
-        File = new JMenu("File");
-        Help = new JMenu("Help");
-        Open = new JMenuItem("Open");
-        Exit = new JMenuItem("Exit");
-        About = new JMenuItem("About");
-        add(File);
-        add(Help);
-        File.add(Open);
-        File.add(Exit);
-        Help.add(About);
-    }
-    //getter method
-    public static Menu getInstance()
+    File = new JMenu("File");
+    Help = new JMenu("Help");
+    Open = new JMenuItem("Open");
+    Exit = new JMenuItem("Exit");
+    About = new JMenuItem("About");
+    add(File);
+    add(Help);
+    File.add(Open);
+    File.add(Exit);
+    Help.add(About);
+  }
+  //getter method
+  public static Menu getInstance()
+  {
+    if(instance==null)
     {
-        if(instance==null)
-        {
-            instance=new Menu();
-        }
-        return instance;
+      instance=new Menu();
     }
+    return instance;
+  }
 
 }
