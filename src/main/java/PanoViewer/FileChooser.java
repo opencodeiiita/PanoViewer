90a1bafe74d90a4999ca052d8c7aee9f8a377866
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-06 19:26:38 +0530

    Modified the FileChooser Class

--- src/main/java/PanoViewer/FileChooser.java
+++ src/main/java/PanoViewer/FileChooser.java
@@ -7,22 +7,22 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser {
 
-    private JFileChooser fileChooser;
-    private FileFilter fileFilter;
-    private static FileChooser instance = new FileChooser();
+  private JFileChooser fileChooser;
+  private FileFilter fileFilter;
+  private static FileChooser instance = new FileChooser();
 
-    public static FileChooser getInstance() {
+  public static FileChooser getInstance() {
         return instance;
     }
 
-    private FileChooser() {
-        String dir = System.getProperty("user.dir");
-        fileChooser = new JFileChooser(dir);
-        fileFilter = new FileNameExtensionFilter("Images Files", ImageIO.getReaderFileSuffixes());
-    }
+  private FileChooser() {
+    String dir = System.getProperty("user.dir");
+    fileChooser = new JFileChooser(dir);
+    fileFilter = new FileNameExtensionFilter("Images Files", ImageIO.getReaderFileSuffixes());
+  }
 
-    public JFileChooser chooser() {
-        fileChooser.setFileFilter(fileFilter);
-        return fileChooser;
+  public JFileChooser chooser() {
+     fileChooser.setFileFilter(fileFilter);
+     return fileChooser;
     }
 }
