bec8986d34c15bd53c9a75af2afa483787480f7c
bipulkmr-crypto <bipul1707@gmail.com>
2021-10-06 14:14:32 +0530

    Removed extra  menubar object

--- src/main/java/PanoViewer/gui/Menu.java
+++ src/main/java/PanoViewer/gui/Menu.java
@@ -3,7 +3,6 @@ package PanoViewer.gui;
 import javax.swing.*;
 
 public class Menu extends JMenuBar {
-   private JMenuBar menuBar;//making a menubar
     private  JMenu File;//creating menu objects
     private JMenu  Help;//creating menu objects
     private JMenuItem Open, Exit;//creating menuitem objects
@@ -11,14 +10,14 @@ public class Menu extends JMenuBar {
     private static Menu instance;//creating a menu instance
     //private constructor for implementing singleton design principle
     private Menu() {
-        menuBar=new JMenuBar();
+//        menuBar=new JMenuBar();
         File = new JMenu("File");
         Help = new JMenu("Help");
         Open = new JMenuItem("Open");
         Exit = new JMenuItem("Exit");
         About = new JMenuItem("About");
-        menuBar.add(File);
-        menuBar.add(Help);
+        instance.add(File);
+        instance.add(Help);
         File.add(Open);
         File.add(Exit);
         Help.add(About);
