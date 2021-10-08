7b1fbf011aaf25858a372fc00d5ae3b63f2942ca
Kishan Tripathi <kshantripathi2011@gmail.com>
2021-10-08 23:00:16 +0530

    Fix imports, remove deprecated keysrtokes

--- src/main/java/PanoViewer/gui/Menu.java
+++ src/main/java/PanoViewer/gui/Menu.java
@@ -1,15 +1,19 @@
 package PanoViewer.gui;
+
+import PanoViewer.Utils.IOUtils;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Menu extends JMenuBar {
-  private  JMenu File;//creating menu objects
-  private JMenu  Help;//creating menu objects
+
+  private JMenu File;//creating menu objects
+  private JMenu Help;//creating menu objects
   private JMenuItem Open, Exit;//creating menuitem objects
   private JMenuItem About;//creating menuitem objects
   private static Menu instance;//creating a menu instance
   //private constructor for implementing singleton design principle
+
   private Menu() {
 //        menuBar=new JMenuBar();
     File = new JMenu("File");
@@ -20,14 +24,13 @@ public class Menu extends JMenuBar {
     add(File);
     add(Help);
     //  shortcut to Open menu item (ctrl + F)
-    Open.setAccelerator(KeyStroke.getKeyStroke('F', Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask()));
+    Open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
 
     // actionlistener to the Open menu item calling chooseFile() static method from IOUtils class
-    Open.addActionListener(new ActionListener()
-    {
+    Open.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        chooseFile();
+        IOUtils.chooseFile();
       }
 
     });
@@ -35,14 +38,13 @@ public class Menu extends JMenuBar {
     File.add(Exit);
     Help.add(About);
   }
+
   //getter method
-  public static Menu getInstance()
-  {
-    if(instance==null)
-    {
-      instance=new Menu();
+  public static Menu getInstance() {
+    if (instance == null) {
+      instance = new Menu();
     }
     return instance;
   }
-   
+
 }
