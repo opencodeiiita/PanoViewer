ae3f5110542b7912a6b7e5f410328d421b7698ee
Puja Saraf <75802818+Puja-Saraf@users.noreply.github.com>
2021-10-09 22:02:08 +0530

    action for exiting app

--- src/main/java/PanoViewer/gui/Menu.java
+++ src/main/java/PanoViewer/gui/Menu.java
@@ -34,6 +34,15 @@ public class Menu extends JMenuBar {
       }
 
     });
+
+    Exit.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+      }
+
+    });
+
     File.add(Open);
     File.add(Exit);
     Help.add(About);
