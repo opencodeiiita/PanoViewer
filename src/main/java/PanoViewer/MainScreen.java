386c87a488ee2f8be7e1ca5d4c055dc5acd9ca8a
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-10 20:00:25 +0530

    Update MainScreen.java

--- src/main/java/PanoViewer/MainScreen.java
+++ src/main/java/PanoViewer/MainScreen.java
@@ -15,16 +15,18 @@ public class MainScreen extends JFrame {
   }
 
   private MainScreen() {
-    setSize(600,600);
+    setSize(600, 600);
     setLayout(null);
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setJMenuBar(menuBar);
     jPanel = new JPanel();
-    jPanel.setBounds(50,50,400,400);
+    jPanel.setBounds(50, 50, 400, 400);
     jPanel.setBackground(Color.GRAY);
     add(jPanel);
   }
 
+  public static void main(String args[]) {
+    MainScreen.getInstance();
+  }
 }
-
