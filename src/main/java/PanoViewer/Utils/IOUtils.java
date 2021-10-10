1fcf4a9eb6190d777aab0eed939a4e3766329d86
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-10 20:01:53 +0530

    Update IOUtils.java

--- src/main/java/PanoViewer/Utils/IOUtils.java
+++ src/main/java/PanoViewer/Utils/IOUtils.java
@@ -3,17 +3,74 @@
  */
 package PanoViewer.Utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.lang.model.type.NullType;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import java.awt.image.BufferedImage;
+import PanoViewer.FileChooser;
+import PanoViewer.MainScreen;
+import PanoViewer.SwitchModes;
 
 /**
  *
  * @author kshan
  */
 public class IOUtils {
-  
-  public static void chooseFile()
-  {
-    
+  /*
+   * Gets the image selected by the user and sets it on the SwitchMode panel.
+   */
+  public static void getFile() {
+    File file = chooseFile();
+    if (file != null) {
+      BufferedImage loadImage = loadImage(file);
+      if (loadImage == null) {
+        return;
+      }
+      SwitchModes.setImage(loadImage);
+    }
+  }
+
+  /*
+   * Loads image from the given file.
+   * 
+   * @param file The file to be opened.
+   * 
+   * @return the opened image. returns null when file is invalid or an exception
+   * occurs.
+   */
+  private static BufferedImage loadImage(File file) {
+
+    if (file == null) {
+      return null;
+    }
+    try {
+      BufferedImage img = ImageIO.read(file);
+      return img;
+    } catch (IOException e) {
+      JOptionPane.showMessageDialog(null, "Load Image <" + file.getName() + "> Failed!", "Error",
+          JOptionPane.ERROR_MESSAGE);
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  /*
+   * Displays a file chooser and returns the file selected by the user.
+   */
+  public static File chooseFile() {
+    JFileChooser fileChooser = FileChooser.getFileChooser();
+    int returnVal = fileChooser.showOpenDialog(MainScreen.getInstance());
+    if (returnVal == JFileChooser.APPROVE_OPTION && fileChooser.getSelectedFile() != null) {
+      File file = fileChooser.getSelectedFile();
+      return file;
+    }
+    return null;
   }
 
   public static InputStream getFileFromResourceAsStream(String fileName) {
