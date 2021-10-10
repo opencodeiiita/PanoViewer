94852e0cda0fddc359c04b19c0f17c73c2e0a985
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-10 20:02:26 +0530

    Update Menu.java

--- src/main/java/PanoViewer/gui/Menu.java
+++ src/main/java/PanoViewer/gui/Menu.java
@@ -7,15 +7,15 @@ import javax.swing.*;
 
 public class Menu extends JMenuBar {
 
-  private JMenu File;//creating menu objects
-  private JMenu Help;//creating menu objects
-  private JMenuItem Open, Exit;//creating menuitem objects
-  private JMenuItem About;//creating menuitem objects
-  private static Menu instance;//creating a menu instance
-  //private constructor for implementing singleton design principle
+  private JMenu File;// creating menu objects
+  private JMenu Help;// creating menu objects
+  private JMenuItem Open, Exit;// creating menuitem objects
+  private JMenuItem About;// creating menuitem objects
+  private static Menu instance;// creating a menu instance
+  // private constructor for implementing singleton design principle
 
   private Menu() {
-//        menuBar=new JMenuBar();
+    // menuBar=new JMenuBar();
     File = new JMenu("File");
     Help = new JMenu("Help");
     Open = new JMenuItem("Open");
@@ -23,14 +23,15 @@ public class Menu extends JMenuBar {
     About = new JMenuItem("About");
     add(File);
     add(Help);
-    //  shortcut to Open menu item (ctrl + F)
+    // shortcut to Open menu item (ctrl + F)
     Open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
 
-    // actionlistener to the Open menu item calling chooseFile() static method from IOUtils class
+    // actionlistener to the Open menu item calling chooseFile() static method from
+    // IOUtils class
     Open.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        IOUtils.chooseFile();
+        IOUtils.getFile();
       }
 
     });
@@ -48,7 +49,7 @@ public class Menu extends JMenuBar {
     Help.add(About);
   }
 
-  //getter method
+  // getter method
   public static Menu getInstance() {
     if (instance == null) {
       instance = new Menu();
