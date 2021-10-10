dd5d85fe122f747dd728ce111d44571c9382e4d9
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-10 20:01:13 +0530

    Update SwitchModes.java

--- src/main/java/PanoViewer/SwitchModes.java
+++ src/main/java/PanoViewer/SwitchModes.java
@@ -1,9 +1,11 @@
 package PanoViewer;
+
 import PanoViewer.ImagePanels.FlatPanel;
 import PanoViewer.ImagePanels.PanoramicPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /*
   @author - Rohan Babbar
@@ -19,8 +21,8 @@ public class SwitchModes extends JFrame {
   private ImagePanel currentMode;
 
   public static SwitchModes getInstance() {
-        return instance;
-    }
+    return instance;
+  }
 
   public ImagePanel getCurrentMode() {
     return currentMode;
@@ -31,31 +33,39 @@ public class SwitchModes extends JFrame {
   }
 
   private SwitchModes() {
-    setSize(600,600);
+    setSize(600, 600);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
     mainPanel = new JPanel(new CardLayout());
     PanoramicPanel panoramicPanel = PanoramicPanel.getInstance();
     FlatPanel flatPanel = FlatPanel.getInstance();
-    mainPanel.add(ImagePanel.FlatImages.toString(),flatPanel);
-    mainPanel.add(ImagePanel.PanoramicImages.toString(),panoramicPanel);
+    mainPanel.add(ImagePanel.FlatImages.toString(), flatPanel);
+    mainPanel.add(ImagePanel.PanoramicImages.toString(), panoramicPanel);
     add(mainPanel);
     cardLayout = (CardLayout) mainPanel.getLayout();
     currentMode = ImagePanel.FlatImages;
-    cardLayout.show(mainPanel,currentMode.toString());
+    cardLayout.show(mainPanel, currentMode.toString());
   }
 
   public void switchingModes(ImagePanel imagePanel) {
     switch (imagePanel) {
       case FlatImages:
         currentMode = ImagePanel.FlatImages;
-        cardLayout.show(mainPanel,currentMode.toString());
+        cardLayout.show(mainPanel, currentMode.toString());
         break;
-    case PanoramicImages:
+      case PanoramicImages:
         currentMode = ImagePanel.PanoramicImages;
-        cardLayout.show(mainPanel,currentMode.toString());
+        cardLayout.show(mainPanel, currentMode.toString());
         break;
     }
   }
-}
 
+  /*
+   * Sets the image to be displayed.
+   * 
+   * @param image the image to be set.
+   */
+  public static void setImage(BufferedImage image) {
+
+  }
+}
