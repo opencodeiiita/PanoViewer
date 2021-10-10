42c6ece5726b4939b1ef25da2bc93d1d2c04bb7e
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-10 10:48:45 +0530

    Change the main function of the app to display the MainScreen class instance

--- src/main/java/PanoViewer/MainScreen.java
+++ src/main/java/PanoViewer/MainScreen.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class MainScreen extends JFrame {
 
   private JMenuBar menuBar = Menu.getInstance();
-  private JPanel jPanel;
+  public static JPanel jPanel;
   private static MainScreen instance = new MainScreen();
 
   public static MainScreen getInstance() {
@@ -16,12 +16,12 @@ public class MainScreen extends JFrame {
 
   private MainScreen() {
     setSize(600,600);
-    setLayout(new FlowLayout());
+    setLayout(null);
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setJMenuBar(menuBar);
     jPanel = new JPanel();
-    jPanel.setBounds(50,50,400,400);
+    jPanel.setBounds(50,50,300,400);
     jPanel.setBackground(Color.GRAY);
     add(jPanel);
   }
