166e54d6fe833a72349cf47a77a728a5b2bd5511
medhatiwari <medhavns1@gmail.com>
2021-10-14 18:02:36 +0530

    approved proposed changes

--- src/main/java/PanoViewer/gui/AboutDialog.java
+++ src/main/java/PanoViewer/gui/AboutDialog.java
@@ -26,7 +26,7 @@ public class AboutDialog extends JPanel {
 
   private AboutDialog(JFrame owner) {
     setLayout(new GridBagLayout());
-    String filePath = "Images/Logos/logo.png";
+    String filePath = "Images/Logos/Panoviewer.png";
     InputStream image = getFileFromResourceAsStream(filePath);
     ImageIcon icon = null;
     try {
@@ -43,20 +43,22 @@ public class AboutDialog extends JPanel {
     }
     GridBagConstraints gbc = new GridBagConstraints();
     assert icon != null;
-    icon = scaleImage(icon, icon.getIconWidth() / 3, icon.getIconHeight() / 3);
-    lab = new JLabel("<html> &nbsp PanoViewer-OpenGl based Image Viewer for 360 &nbsp <br>&nbsp degree Panoramic Images &nbsp <br><br> &nbsp Version 1.0<br><br>&nbsp Minimum requirements for the App to run JDK 8.0+ &nbsp <br> &nbsp and OpenGl 4.0+</html>&nbsp", icon, JLabel.CENTER);
+    icon = scaleImage(icon, icon.getIconWidth(), icon.getIconHeight());
+    lab = new JLabel("<html> <br><br><br>&nbsp PanoViewer-OpenGl based Image Viewer for 360 &nbsp <br>&nbsp degree Panoramic Images &nbsp <br><br> &nbsp Version 1.0<br><br>&nbsp Minimum requirements for the App to run JDK 8.0+ &nbsp <br> &nbsp and OpenGl 4.0+</html>", icon, JLabel.CENTER);
     gbc.gridy = 0;
     gbc.gridx = 0;
     add(lab, gbc);
     lab.setVerticalTextPosition(JLabel.TOP);
     lab.setHorizontalAlignment(JLabel.CENTER);
     lab.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+    lab.setForeground(new Color(0xffffff));
     but = new JButton("Source Code");
     but.addActionListener(actionEvent -> open(url));
     gbc.gridy = 1;
     gbc.gridx = 0;
     add(but, gbc);
-    setBackground(Color.LIGHT_GRAY);
+
+    setBackground(Color.black);
   }
 
   /*
