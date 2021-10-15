4c8cdaf6c4aba5089b8bca798cb5c388135c2b70
Kishan Tripathi <kshantripathi2011@gmail.com>
2021-10-15 13:16:45 +0530

    Use latest deployment of jogl

--- src/main/java/PanoViewer/PanoViewer.java
+++ src/main/java/PanoViewer/PanoViewer.java
@@ -3,6 +3,15 @@
  */
 package PanoViewer;
 
+import PanoViewer.gui.PhotoSphere;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 /**
  *
  * @author kshan
@@ -13,6 +22,22 @@ public class PanoViewer {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    MainScreen mainScreen = MainScreen.getInstance();
+    if (args.length < 1) {
+      return;
+    }
+    BufferedImage img;
+    try {
+      img = ImageIO.read(new File(args[0]));
+    } catch (IOException ex) {
+      Logger.getLogger(PanoViewer.class.getName()).log(Level.SEVERE, null, ex);
+      return;
+    }
+    JFrame jFrame = new JFrame("PanoViewer");
+    jFrame.setSize(600, 600);
+    PhotoSphere ps = new PhotoSphere();
+    jFrame.add(ps);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jFrame.setVisible(true);
+    ps.replaceImage(img);
   }
 }
