9c05ef4a005575be9ba3125f197c652351df92e3
bipulkmr-crypto <72005385+bipulkmr-crypto@users.noreply.github.com>
2021-10-15 11:06:55 +0530

    Added java docs

--- src/main/java/PanoViewer/gui/ImageViewer.java
+++ src/main/java/PanoViewer/gui/ImageViewer.java
@@ -2,12 +2,39 @@ package PanoViewer.gui;
 
 import java.awt.image.BufferedImage;
 
-public abstract class ImageViewer {
-  public abstract void setImage(BufferedImage image);
-  public abstract void enableZoom();
-  public abstract void disableZoom();
-  public abstract boolean isZoomEnabled();
-  public abstract void enablePannig();
-  public abstract void disablePannig();
-  public abstract boolean isPanningEnabled();
+public interface ImageViewer {
+    /*
+     void setImage(Buffered Image)-
+     returns- void
+
+     */
+      void setImage(BufferedImage image);
+    /*
+    void enableZoom()-Enables zoom feature
+     */
+    void enableZoom();
+    /*
+    void disableZoom- disables zoom feature
+     */
+     void disableZoom();
+    /*
+    Checks if the zoom is enabled or not.
+    Returns true is zoom is enabled else false.
+     */
+     boolean isZoomEnabled();
+    /*
+    Checks if the Panning is enabled or not.
+    Returns true is Panning is enabled else false.
+     */
+     boolean isPanningEnabled();
+    /*
+    Enables panning
+     */
+     void enablePanning();
+    /*
+    Disables panning
+     */
+     void disablePanning();
+
+
 }
