5c005b93428ace199d06fb60598e4f434f059768
Kishan Tripathi <kshantripathi2011@gmail.com>
2021-10-16 10:03:29 +0530

    Make separate interfaces for panning and zooming. Make abstract class for Opengl based Image viewers.

--- src/main/java/PanoViewer/gui/ImageViewer.java
+++ src/main/java/PanoViewer/gui/ImageViewer.java
@@ -2,40 +2,16 @@ package PanoViewer.gui;
 
 import java.awt.image.BufferedImage;
 
-public interface ImageViewer {
-    /**
-     * Sets the Current Image
-     * @param image The image to set
-     */
+/**
+ * Image Viewer with zooming and panning for navigation.
+ * @author kshan
+ */
+public interface ImageViewer extends Zoomable, Pannable {
 
-    void setImage(BufferedImage image);
-    /**
-     * Enables or disables zoom
-     *
-     * @param flag if true enables zoom else disables it
-     */
-
-    void enableZoom(boolean flag);
-    /**
-     * Checks if zoom is enabled or not.
-     *
-     * @return {@code true} if the zooming is enabled,  {@code false} otherwise.
-     */
-
-    boolean isZoomEnabled();
-    /**
-     * Checks if panning is enabled or not.
-     *
-     * @return {@code true} if the zooming is enabled,  {@code false} otherwise.
-     */
-
-    boolean isPanningEnabled();
-    /**
-     * Enables or disables Panning
-     *
-     * @param flag if true enables panning else disables it
-     */
-
-    void enablePanning(boolean flag);
-
+  /**
+   * Sets the Current Image
+   *
+   * @param image The image to set
+   */
+  void setImage(BufferedImage image);
 }
