4980552dbb5a39e1f54ec84a162aa172e99f089a
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-18 14:17:02 +0530

    Update ModeRecorder.java

--- src/main/java/PanoViewer/ModeRecorder.java
+++ src/main/java/PanoViewer/ModeRecorder.java
@@ -4,14 +4,28 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class ModeRecorder {
-    private PropertyChangeSupport support;
+    private static PropertyChangeSupport support;
     private Mode currentMode;
-
-    public ModeRecorder() {
-        support = new PropertyChangeSupport(this);
+    private ModeRecorder instance;
+    private ModeRecorder()
+    {
+    }
+    public ModeRecorder getInstance()
+    {
+        if(instance==null)
+        {
+            instance=new ModeRecorder();
+            support=new PropertyChangeSupport(this);
+        }
+        return instance;
+    }
+    
+    public Mode getCurrentMode()
+    {
+        return currentMode;
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener ls) {
+    public static void addPropertyChangeListener(PropertyChangeListener ls) {
         support.addPropertyChangeListener(ls);
 
     }
@@ -19,9 +33,16 @@ public class ModeRecorder {
     public void removePropertyChangeListener(PropertyChangeListener ls) {
         support.removePropertyChangeListener(ls);
     }
-
+    /**
+     * @var-if mode md is Panoramic  then flag is false  else true hence a property change is propagated
+     * @param  md -mode to be set
+     *
+     *
+     *
+     */
     public void setMode(Mode md) {
         boolean flag = md != Mode.Panoramic;
         support.firePropertyChange("mode", flag, !flag);
+        currentMode=md;
     }
 }
