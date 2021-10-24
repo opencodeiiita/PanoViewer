9017698c23d2ed7c1cc1057a90559ba19f647cee
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-24 16:46:30 +0530

    Added Flat Image Viewer

--- src/main/java/PanoViewer/ImagePanels/FlatPanel.java
+++ src/main/java/PanoViewer/ImagePanels/FlatPanel.java
@@ -1,20 +1,107 @@
 package PanoViewer.ImagePanels;
 
-import javax.swing.*;
+import PanoViewer.gui.JOGLImageViewer;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureCoords;
+import com.jogamp.opengl.util.texture.TextureData;
+import com.jogamp.opengl.util.texture.TextureIO;
+import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
+import java.awt.image.BufferedImage;
 
-/*
+/**
   @author - Rohan Babbar
   The Flat Image panel which will display Flat Images
 */
 
-public class FlatPanel extends JPanel {
+public class FlatPanel extends JOGLImageViewer {
 
+  private static GL2 gl;
+  private BufferedImage baseImage;
+  private TextureData textureData;
+  private Texture texture;
   private static FlatPanel instance = new FlatPanel();
 
   private FlatPanel() {
+
   }
 
-    public static FlatPanel getInstance() {
-        return instance;
-    }
+  @Override
+  public void setImage(BufferedImage image) {
+    baseImage = image;
+  }
+
+  @Override
+  public boolean isPanningEnabled() {
+    return false;
+  }
+
+  @Override
+  public void enablePanning(boolean enable) {
+
+  }
+
+  @Override
+  public void pan(float panX, float panY) {
+
+  }
+
+  @Override
+  public void enableZoom(boolean enable) {
+
+  }
+
+  @Override
+  public boolean isZoomEnabled() {
+    return false;
+  }
+
+  @Override
+  public void zoom(float zoomBy) {
+
+  }
+
+  @Override
+  public void init(GLAutoDrawable drawable) {
+    gl = drawable.getGL().getGL2();
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+    textureData = AWTTextureIO.newTextureData(gl.getGLProfile(), baseImage, false);
+    texture = TextureIO.newTexture(textureData);
+  }
+
+  @Override
+  public void dispose(GLAutoDrawable drawable) {
+    texture.disable(gl);
+    texture.destroy(gl);
+  }
+
+  @Override
+  public void display(GLAutoDrawable drawable) {
+    texture.enable(gl);
+    texture.bind(gl);
+    TextureCoords coordinates = texture.getImageTexCoords();
+    gl.glBegin(GL2.GL_QUADS);
+    gl.glTexCoord2f(coordinates.left(), coordinates.bottom());
+    gl.glVertex3f(0, 0, 0);
+    gl.glTexCoord2f(coordinates.right(), coordinates.bottom());
+    gl.glVertex3f(1, 0, 0);
+    gl.glTexCoord2f(coordinates.right(), coordinates.top());
+    gl.glVertex3f(1, 1, 0);
+    gl.glTexCoord2f(coordinates.left(), coordinates.top());
+    gl.glVertex3f(0, 1, 0);
+    gl.glEnd();
+  }
+
+  @Override
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    gl.glViewport(0,0,width,height);
+    gl.glTexEnvi(GL2.GL_TEXTURE_ENV, GL2.GL_TEXTURE_ENV_MODE, GL2.GL_REPLACE);
+    repaint();
+  }
+
+  public static FlatPanel getInstance() {
+    return instance;
+  }
 }
