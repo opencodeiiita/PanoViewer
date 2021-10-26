c87823555a610ffc696bf122d26deb25c98783b2
Moto <76700116+KIRTEE-TECH@users.noreply.github.com>
2021-10-26 13:52:38 +0530

    Edited Test

--- src/test/java/cameraunittest.java
+++ src/test/java/cameraunittest.java
@@ -22,27 +22,53 @@ public class cameraunittest {
     assertEquals(true,camera_new.getYaw()-(0)<epsilon);
   }
 
-   @Test
-   public void rotateY_test(){
-      Camera rotate_new = new Camera();
-      rotate_new.rotateY(3);
-      double yaw = rotate_new.getYaw();
-      assertEquals(true, yaw>=0 && yaw<=2*Math.PI);
-   }
-
-   @Test
-   public void rotateAlongPitchAxis_test(){
-     Camera rotatealongpitchaxis = new Camera();
-     rotatealongpitchaxis.rotateAlongPitchAxis(9);
-     double outputaxis = rotatealongpitchaxis.getPitch();
-     assertEquals(1.57,outputaxis,0.001);
-   }
+//   @Test
+//   public void rotateY_test(){
+//      Camera rotate_new = new Camera();
+//      rotate_new.rotate(3.0f, 6.0f);
+//      double yaw = rotate_new.getYaw();
+//      assertEquals(true, yaw>=0 && yaw<=2*Math.PI);
+//   }
+//
+//   @Test
+//   public void rotateAlongPitchAxis_test(){
+//     Camera rotatealongpitchaxis = new Camera();
+//     rotatealongpitchaxis.rotateAlongPitchAxis(9.6f,8.9f);
+//     double outputaxis = rotatealongpitchaxis.getPitch();
+//     assertEquals(1.57,outputaxis,0.001);
+//   }
 
    @Test
    public void rotate_test(){
      Camera rotate_test = new Camera();
-     rotate_test.rotate(-7,2);
-     assertEquals(false, rotate_test.getPitch() >= Math.PI/2 && rotate_test.getPitch()<-Math.PI/2);
+     double epsilon = 1e-5;
+     double yaw = rotate_test.getYaw();
 
-   }
+     rotate_test.rotate((float) (Math.PI/4), (float) (Math.PI/4));
+     assertTrue(rotate_test.getYaw() <= 2*Math.PI+epsilon );
+     assertTrue(rotate_test.getYaw() >= 0 -epsilon );
+     assertEquals((float)(Math.PI/4),rotate_test.getYaw(),epsilon);
+     assertTrue(Math.abs(rotate_test.getPitch()-Math.PI/4)<epsilon);
+
+     rotate_test.rotate((float)(3*Math.PI/4), (float) (Math.PI/2));
+     assertTrue(rotate_test.getYaw() <= 2*Math.PI+epsilon );
+     assertTrue(rotate_test.getYaw() >= 0 -epsilon );
+     assertTrue(rotate_test.getPitch() <= Math.PI/2+epsilon);
+     assertTrue(rotate_test.getPitch() >= -Math.PI/2-epsilon );
+     assertTrue(rotate_test.getYaw()+epsilon>=(float)(3/2*Math.PI));
+     assertTrue(rotate_test.getYaw()-epsilon<=(float)(3/2*Math.PI));
+     assertTrue(rotate_test.getPitch()+epsilon>=(float)(Math.PI/2));
+     assertTrue(rotate_test.getPitch()-epsilon<=(float)(Math.PI/2));
+
+     rotate_test.rotate((float) (4/3*Math.PI), (float) (7/4*Math.PI));
+     assertTrue(rotate_test.getYaw() <= 2*Math.PI+epsilon );
+     assertTrue(rotate_test.getYaw() >= 0 -epsilon );
+     assertTrue(rotate_test.getYaw()+epsilon>=(float)(4/6*Math.PI));
+     assertTrue(rotate_test.getYaw()-epsilon<=(float)(4/6*Math.PI));
+     assertTrue(rotate_test.getPitch() <= Math.PI/2+epsilon );
+     assertTrue(rotate_test.getPitch() >= -Math.PI/2-epsilon );
+
+
+
+        }
 }
